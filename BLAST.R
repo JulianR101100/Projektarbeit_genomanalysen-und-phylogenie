f71b73a5560b005eb98761f0bc12cbc11dcc7a19
jolordi <johannvogel06@gmail.com>
2026-01-09 20:26:36 +0100

    noch ein versuch

--- BLAST.R
+++ BLAST.R
@@ -19,7 +19,7 @@ wds <- data.frame(
   path = c("C:/Users/julia/OneDrive/Integrated Life Siences/Genomanalysen und Phylogenie/Projektarbeit",
            "/home/stud/ha24vepa/Documents/Bash_Linux_Introduction_supplements/Genom_und_Phylogenie_kurs/...",
            "C:/Users/johan/Uni/Genomanalyse_Projekt",
-           "Rphi: Copy paste dein Working directory"),
+           "Raphi: Copy paste dein Working directory"),
   stringsAsFactors = FALSE
 )
 for (i in seq_len(nrow(wds))) {
