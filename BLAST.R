f0b0a1aa0523b8eeeda216d92e3693d26b732650
jolordi <johannvogel06@gmail.com>
2026-01-09 21:45:03 +0100

    änderung meiner wd

--- BLAST.R
+++ BLAST.R
@@ -18,7 +18,7 @@ wds <- data.frame(
              "Rphi ..."),
   path = c("C:/Users/julia/OneDrive/Integrated Life Siences/Genomanalysen und Phylogenie/Projektarbeit",
            "/home/stud/ha24vepa/Documents/Bash_Linux_Introduction_supplements/Genom_und_Phylogenie_kurs/...",
-           "C:/Users/johan/Uni/Genomanalyse_Projekt",
+           "C:/Users/johan/Uni/Genomanalyse_Projekt/Projektarbeit",
            "C:/Users/darko/OneDrive/Dokumente/Genomanalysen_und_Phylogenie_Projekt"),
   stringsAsFactors = FALSE
 )
