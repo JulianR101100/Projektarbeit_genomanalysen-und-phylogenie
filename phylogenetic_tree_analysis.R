c78186776a71cac648ed382c689b6ad34bdbd3ba
Raphi036 <raphael.ortner@magenta.de>
2026-03-03 14:40:17 +0100

    Zeichen verbessert

--- phylogenetic_tree_analysis.R
+++ phylogenetic_tree_analysis.R
@@ -20,7 +20,7 @@ wds <- data.frame(
   path = c("C:/Users/julia/OneDrive/Integrated Life Siences/Genomanalysen und Phylogenie/Projektarbeit",
            "/home/stud/ha24vepa/Documents/Bash_Linux_Introduction_supplements/Genom_und_Phylogenie_kurs/...",
            "C:/Users/johan/Uni/Genomanalyse_Projekt/Projektarbeit",
-           "C:\Users\darko\OneDrive\Dokumente\Genomanalysen_und_Phylogenie_Projekt\Projektarbeit_genomanalysen-und-phylogenie"),
+           "C:/Users/darko/OneDrive/Dokumente/Genomanalysen_und_Phylogenie_Projekt/Projektarbeit_genomanalysen-und-phylogenie"),
   stringsAsFactors = FALSE
 )
 for (i in seq_len(nrow(wds))) {
